8cafda46c77cc1b258eff41a167950e7f646f8e4
Edjufo-NUD <formillezaej@students.nu-dasma.edu.ph>
2025-09-19 14:23:46 +0800

    Initial commit with secure environment variables

--- webcap/src/supabasebaseClient.js
+++ webcap/src/supabasebaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Replace with your own Supabase URL and anon key
-const SUPABASE_URL = "https://vydfizldgyaxheceslzy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5ZGZpemxkZ3lheGhlY2VzbHp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4ODQ2MjQsImV4cCI6MjA2MzQ2MDYyNH0._BKPiyRefCfuCE4aqE3M1EeaZkGTkVl-Y6z-f15Y-IY";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_KEY;
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
